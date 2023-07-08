4b06413a2c618b67d8027ca8848ab0231703ef9c
Rodolfo Rivera <rorivera0621@gmail.com>
2023-07-08 14:47:35 -0700

    got bubble sort to work + visuals

--- src/SortingPage.jsx
+++ src/SortingPage.jsx
@@ -2,16 +2,49 @@ import PropTypes from 'prop-types'
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import React, { Component } from 'react'
+import {bubbleSort} from './bubbleSort'
 
 function SortingPage(){
     const navigate = useNavigate();
   const [lines, setLines] = useState([]);
-  let widths = 5;
   let idx = 0;
   useEffect(() => {
-    resetArray();
-    console.log(lines);
+    resetArray()
   }, []);
+    return (
+      <>
+        <header className="header">
+          <a className="logo" onClick={() => navigate("/")}>
+            Algorithms Visualizer
+          </a>
+          <nav className="navbar">
+            <a onClick={() => navigate("/sorting")}>Sorting Algorithms</a>
+            <a onClick={() => navigate("/pathfinding")}>
+              Pathfinding Algorithms
+            </a>
+          </nav>
+        </header>
+        <div className="container">
+          {lines.length > 0 &&
+            lines.map((line, idx) => (
+              <div
+                key={idx}
+                id={idx++}
+                className="line"
+                style={{
+                  height: `${line * 1.3 + 30}px`,
+                  padding: `0px ${getWidth()}px`,
+                }}
+              ></div>
+            ))}
+
+          <button className="bubble" onClick={bubble}>
+            Bubble Sort
+          </button>
+        </div>
+      </>
+    );
+  
 
   function resetArray(length) {
     const arr = [];
@@ -39,76 +72,12 @@ function SortingPage(){
   }
   function deletes(){
     let length = lines.length-1;
-    console.log(lines[0].key)
     setLines(lines.filter(line => line !== lines[length]))
     
-    
-    
   }
-function swap(){
-  let length = lines.length - 1;
-      if(document.getElementById(length).offsetHeight > document.getElementById(length-1).offsetHeight){
-       document.getElementById(length).style.backgroundColor = 'green';
-       document.getElementById(length-1).style.backgroundColor = "blue";
-      
-      
-    }else{
-      document.getElementById(length).style.backgroundColor = "blue";
-      document.getElementById(length - 1).style.backgroundColor = "green";
+    function bubble(){
+      bubbleSort(lines)
     }
-    if (lines[length] > lines[length-1] ) {
-      let height = document.getElementById(length).offsetHeight;
-      let height2 = document.getElementById(length-1).offsetHeight;
-      document.getElementById(length).style.height = height2 + "px";
-      document.getElementById(length - 1).style.height = height + "px";
-      console.log(lines);
-      let temp = lines[length];
-      lines[length] = lines[length-1];
-      lines[length-1] = temp;
-      console.log(lines)
-    } else {
-      // let height = document.getElementById(length-1).offsetHeight;
-      // let height2 = document.getElementById(length).offsetHeight;
-      // document.getElementById(length - 1).style.height = height2 + "px";
-      // document.getElementById(length ).style.height = height + "px";
-    }
-    }
-    return (
-      <>
-        <header className="header">
-          <a className="logo" onClick={() => navigate("/")}>
-            Algorithms Visualizer
-          </a>
-          <nav className="navbar">
-            <a onClick={() => navigate("/sorting")}>Sorting Algorithms</a>
-            <a onClick={() => navigate("/pathfinding")}>
-              Pathfinding Algorithms
-            </a>
-          </nav>
-        </header>
-        <div className="container">
-          {lines.length > 0 &&
-            lines.map((line, idx) => (
-              <div
-                key={idx}
-                id={idx++}
-                className="line"
-                style={{
-                  height: `${line * 1.3 + 30}px`,
-                  padding: `0px ${getWidth()}px`,
-                }}
-              ></div>
-            ))}
-          <button className="delete" onClick={deletes}>
-            Delete item
-          </button>
-          <button className="delete" onClick={swap}>
-            Swap last 2 items
-          </button>
-        </div>
-      </>
-    );
-  
 }
 
 export default SortingPage
---
+++ src/bubbleSort.js
@@ -0,0 +1,43 @@
+export async function bubbleSort(arr){
+    let swapped = false;
+    for(var i = 0;i<arr.length-1;i++){
+        swapped = false
+        for(var j = 0;j<arr.length-1-i;j++){
+            if(arr[j] > arr[j+1]){ 
+                document.getElementById(j).style.backgroundColor = "green"
+                document.getElementById(j+1).style.backgroundColor = "red" 
+                await delay(50);
+                swap(j, j +1)
+                let temp = arr[j]
+                arr[j]=arr[j +1];
+                arr[j +1] = temp;
+                swapped = true
+            }else{
+                document.getElementById(j).style.backgroundColor = "green";
+                document.getElementById(j +1).style.backgroundColor = "green"; 
+                await delay(50)
+            }
+            
+            document.getElementById(j).style.backgroundColor = "";
+            document.getElementById(j +1).style.backgroundColor = "green";
+            
+        } 
+        if(!swapped){
+            for(let k = 0;k<=i;k++){
+                document.getElementById(k).style.backgroundColor = "green";
+            }
+            break
+        }
+        if(j == 0 && i == arr.length-2)document.getElementById(j).style.backgroundColor = "green";
+    }
+}
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function swap(index1, index2) {
+    var height = document.getElementById(index1).offsetHeight;
+    var height2 = document.getElementById(index2).offsetHeight;
+    document.getElementById(index1).style.height = height2 + "px";
+    document.getElementById(index2).style.height = height + "px";
+}
